42dd97609d4fb9656ff7c0edfacd10a3737a31d9
12oattao21 <111548627+12oattao21@users.noreply.github.com>
2022-12-11 18:36:14 +0700

    comment

--- 1/SpanningTree.py
+++ 1/SpanningTree.py
@@ -2,37 +2,32 @@ from collections import defaultdict
 
 class Graph1:
 
-	def __init__(self, vertices):
-		self.V = vertices # No. of vertices
+	def __init__(self, vertices): # หาจุดยอด 
+		self.V = vertices 
 		self.graph = []
-		# to store graph
 
-	# function to add an edge to graph
-	def addEdge1(self, u, v, w):
+
+	
+	def addEdge1(self, u, v, w): # กำหนดจุดสองจุดและค่าของจุด
 		self.graph.append([u, v, w])
 
-	# A utility function to find set of an element i
-	# (truly uses path compression technique)
-	def find(self, parent, i):
+
+	def find(self, parent, i): #ใช้หาเส้นทางต้นและปลาย 
 		if parent[i] != i:
-		# Reassignment of node's parent to root node as
-		# path compression requires
 			parent[i] = self.find(parent, parent[i])
 		return parent[i]
 
 	# A function that does union of two sets of x and y
 	# (uses union by rank)
-	def union(self, parent, rank, x, y):
-		
-		# Attach smaller rank tree under root of
-		# high rank tree (Union by Rank)
+	def union(self, parent, rank, x, y): # 
+
+		# หาคู่ของจุดยอดสองที่น้อยด้วยการเปรียบเทียบ
 		if rank[x] < rank[y]:
 			parent[x] = y
 		elif rank[x] > rank[y]:
 			parent[y] = x
 
-		# If ranks are same, then make one as root
-		# and increment its rank by one
+		# หาได้แล้วก็หาต่อไป
 		else:
 			parent[y] = x
 			rank[x] += 1
@@ -40,82 +35,36 @@ class Graph1:
 	# The main function to construct MST using Kruskal's
 		# algorithm
 	def KruskalMST(self):
-
-		result = [] # This will store the resultant MST
-
-		# An index variable, used for sorted edges
-		i = 0
-
-		# An index variable, used for result[]
-		e = 0
-
-		# Step 1: Sort all the edges in
-		# non-decreasing order of their
-		# weight. If we are not allowed to change the
-		# given graph, we can create a copy of graph
-		self.graph = sorted(self.graph,
-							key=lambda item: item[2])
-
+		result = []
 		parent = []
 		rank = []
+		i = 0
+		e = 0
+	
+		self.graph = sorted(self.graph,key=lambda item: item[2])
 
-		# Create V subsets with single elements
-		for node in range(self.V):
+		for node in range(self.V):  #ใส่ค่าว่าจุดอะไรบ้าง
 			parent.append(node)
 			rank.append(0)
+	
 
-		# Number of edges to be taken is equal to V-1
-		while e < self.V - 1:
+		while e < self.V - 1: #หาทางที่ไปน้อยที่สุด
 
-			# Step 2: Pick the smallest edge and increment
-			# the index for next iteration
-			u, v, w = self.graph[i]
+			u, v, w = self.graph[i] 
 			i = i + 1
-			x = self.find(parent, u)
-			y = self.find(parent, v)
+			x = self.find(parent, u) #กำหนดค่าจุดเริ่ม
+			y = self.find(parent, v) #กำหนดค่าจุดปลาย
 
-			# If including this edge doesn't
-			# cause cycle, then include it in result
-			# and increment the index of result
-			# for next edge
-			if x != y:
+			if x != y: #ตรวจสอบจนหาทางได้น้อยที่สุด
 				e = e + 1
 				result.append([u, v, w])
 				self.union(parent, rank, x, y)
-			# Else discard the edge
+
 
 		minimumCost = 0
 		print("Edges in the constructed MST")
-		for u, v, weight in result:
+		for u, v, weight in result: 
 			minimumCost += weight
 			print("%d -- %d == %d" % (u, v, weight))
 		print("Minimum Spanning Tree", minimumCost)
-
-
-class Graph2:
-    def __init__(self):
-        self.graph = defaultdict(list)
-    def addEdge2(self,u,v):
-        self.graph[u].append(v)
-    def DFSUtil(self,v,visited):
-        visited.add(v)
-        print(v, end=' ')
-        for neighbour in self.graph[v]:
-            if neighbour not in visited:
-                self.DFSUtil(neighbour,visited)
-    def DFS(self, v):
-        visited = set()
-        self.DFSUtil(v,visited)
-    def BFS(self, s):
-        visited = [False] * (max(self.graph) + 1)
-        queue = []
-        queue.append(s)
-        visited[s] = True
-        while queue:
-            s = queue.pop(0)
-            print (s, end = " ")
-            for i in self.graph[s]:
-                if visited[i] == False:
-                    queue.append(i)
-                    visited[i] = True
-
+ 
